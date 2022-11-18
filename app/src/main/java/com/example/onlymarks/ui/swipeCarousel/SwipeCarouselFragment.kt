be4ed4752a3856f97246012bf793adcd900720df
mark-torres10 <markptorres1@gmail.com>
2022-11-18 16:35:27 -0500

    feat: add X and checkmark on left/right swipe respectively

--- app/src/main/java/com/example/onlymarks/ui/swipeCarousel/SwipeCarouselFragment.kt
+++ app/src/main/java/com/example/onlymarks/ui/swipeCarousel/SwipeCarouselFragment.kt
@@ -5,12 +5,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.onlymarks.dataclasses.SwipeCard
 import com.example.onlymarks.databinding.SwipeCarouselFragmentBinding
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.Direction
+import java.lang.Thread.sleep
 import kotlin.properties.Delegates
 
 class SwipeCarouselFragment: Fragment() {
@@ -18,6 +20,7 @@ class SwipeCarouselFragment: Fragment() {
     private var _binding: SwipeCarouselFragmentBinding? = null
     private lateinit var adapter: SwipeCarouselAdapter
     private lateinit var viewModel: SwipeCarouselViewModel
+    private lateinit var layoutManager: CardStackLayoutManager
 
     private lateinit var currentCard: SwipeCard
     private var latestIndex by Delegates.notNull<Int>()
@@ -26,8 +29,31 @@ class SwipeCarouselFragment: Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private fun setOverlayVisibility(
+        direction: Direction?, ratio: Float
+    ) {
+        // set overlay visibility on card drag depending on direction + ratio.
+        if (direction?.name == "Left") {
+            //val opacity = (ratio * 100).toInt()
+            binding.cardRedX.visibility = View.VISIBLE
+            binding.cardRedX.alpha = ratio
+            binding.cardGreenCheckMark.visibility = View.INVISIBLE
+        } else if (direction?.name == "Right") {
+            val opacity = (ratio * 100).toInt()
+            binding.cardGreenCheckMark.visibility = View.VISIBLE
+            binding.cardGreenCheckMark.alpha = ratio
+            binding.cardRedX.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun clearOverlays() {
+        binding.cardRedX.visibility = View.INVISIBLE
+        binding.cardGreenCheckMark.visibility = View.INVISIBLE
+    }
+
     val cardStackListener = object : CardStackListener {
         override fun onCardDragging(direction: Direction?, ratio: Float) {
+            setOverlayVisibility(direction, ratio)
         }
 
         override fun onCardRewound() {
@@ -37,7 +63,9 @@ class SwipeCarouselFragment: Fragment() {
         }
 
         override fun onCardAppeared(view: View?, position: Int) {
-
+            // TODO: work out the kinks of this delay.
+            sleep(500)
+            clearOverlays()
         }
 
         override fun onCardDisappeared(view: View?, position: Int) {
@@ -47,23 +75,22 @@ class SwipeCarouselFragment: Fragment() {
         }
 
         override fun onCardSwiped(direction: Direction?) {
+            if (layoutManager.topPosition == adapter.itemCount) {
+                Log.d("XXX", "Reached end of stack.")
+                sleep(500)
+                clearOverlays()
+            }
             if (direction == Direction.Right) {
                 // update to liked profile. Update in adapter and view model
                 currentCard.youLikeThemBool = true
                 adapter.getSwipeCardsList()[latestIndex] = currentCard
-                // TODO: will have to update viewModel at some point
-                Log.d("XXX", "Liked? ${adapter.getSwipeCardsList()[latestIndex]}")
                 viewModel.setSwipeCards(adapter.getSwipeCardsList())
-                // TODO: show X popup
 
             } else if (direction == Direction.Left) {
                 // update to disliked profile
                 currentCard.youLikeThemBool = false
                 adapter.getSwipeCardsList()[latestIndex] = currentCard
-                // TODO: will have to update viewModel at some point
-                Log.d("XXX", "Liked? ${adapter.getSwipeCardsList()[latestIndex]}")
                 viewModel.setSwipeCards(adapter.getSwipeCardsList())
-                // TODO: show green checkmark
             }
         }
     }
@@ -85,7 +112,8 @@ class SwipeCarouselFragment: Fragment() {
         val currentSwipeCards = viewModel.observeSwipeCards().value!!
         adapter.updateSwipeCardsList(currentSwipeCards)
 
-        cardStackView.layoutManager = CardStackLayoutManager(this.context, cardStackListener)
+        layoutManager = CardStackLayoutManager(this.context, cardStackListener)
+        cardStackView.layoutManager = layoutManager
         cardStackView.adapter = adapter
 
         return root
